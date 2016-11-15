6fe60618dd8fe55dc0332ccac5ddb78094d2579e
martin <mrosso10@gmail.com>
2016-11-15 16:57:52 -0300

    Estructura inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var slim = require("gulp-slim");
+var sass = require("gulp-sass");
 
 gulp.task('default', function() {
-  // place code for your default task here
 });
 
 gulp.task('hello', function() {
@@ -12,11 +12,52 @@ gulp.task('hello', function() {
 gulp.task('slim', function(){
   gulp.src("./app/views/*.slim")
     .pipe(slim({
-      pretty: true
+      // rails: true,
+      pretty: true,
     }))
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/views/*.slim', ['slim']);
+
+var
+    source = 'app/assets/stylesheets/',
+    dest = 'dist/';
+    
+var bootstrapSass = {
+        in: './node_modules/bootstrap-sass/'
+    };
+
+var fonts = {
+        in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+        out: dest + 'fonts/'
+    };
+
+var scss = {
+    in: source + 'main.scss',
+    out: dest + 'css/',
+    watch: source + '**/*',
+    sassOpts: {
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets']
+    }
+};
+
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
 });
+
+gulp.task('sass', ['fonts'], function () {
+    return gulp.src(scss.in)
+        .pipe(sass(scss.sassOpts))
+        .pipe(gulp.dest(scss.out));
+});
+
+gulp.task('watch', ['sass', 'slim'], function(){
+  gulp.watch('app/views/*.slim', ['slim']);
+  gulp.watch(scss.watch, ['sass']);
+});
+
