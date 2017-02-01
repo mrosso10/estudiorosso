7e4f809013e44e5efd3fe4d18167d13e4fef108a
Martin <mrosso10@gmail.com>
2017-02-01 17:36:52 -0300

    Static

---
+++ static/estudiorosso.programando.com.ar/assets/main-ad741a1efdb488167ba5bb46d9c3de60.js
@@ -0,0 +1,3 @@
+
+jQuery(document).ready(function(){$(document).scroll(function(){var x=$(this).scrollTop();$(".backed").css('background-position','50% '+parseInt(50+x/10)+'%');});$(".flyer").each(function(index,flyer){$(document).scroll(function(){var x=$(this).scrollTop();var offset=$(flyer).offset()['top'];$(flyer).css('background-position','50% '+parseInt(50+(x-offset)/10)+'%');});});});function go_top(){$('html, body').animate({scrollTop:0},1000);}
+function go_map(){$('html, body').animate({scrollTop:$('.mapa').offset()['top']},1000);}
