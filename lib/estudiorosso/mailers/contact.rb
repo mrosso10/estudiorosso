0d63071df716ff8caf9b1ef75e2076318a15f96f
martin <mrosso10@gmail.com>
2017-02-02 15:54:18 -0300

    Mail corregido

--- lib/estudiorosso/mailers/contact.rb
+++ lib/estudiorosso/mailers/contact.rb
@@ -2,7 +2,7 @@ class Mailers::Contact
   include Hanami::Mailer
 
   from    'web@estudiorosso.com.ar'
-  to      'mrosso10@gmail.com'
+  to      'mrosso10@gmail.com; mariana@estudiorosso.com.ar'
   subject 'Email desde Web'
   templates
 end
