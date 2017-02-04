75267039defb4974d33ed801064d26d76766094b
Martin <mrosso10@gmail.com>
2017-02-04 17:34:42 -0300

    Mail

--- lib/estudiorosso/mailers/contact.rb
+++ lib/estudiorosso/mailers/contact.rb
@@ -2,7 +2,7 @@ class Mailers::Contact
   include Hanami::Mailer
 
   from    'web@estudiorosso.com.ar'
-  to      'mariana@estudiorosso.com.ar'
+  to      'mrosso10@gmail.com'
   subject 'Email desde Web'
   templates
 end
